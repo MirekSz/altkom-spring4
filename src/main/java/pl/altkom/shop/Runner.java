145fa715aaf1d74cdee21ac9be32a19b1afbe3f4
MirekSz <m.szajowski@gmail.com>
2018-04-27 12:17:48 +0200

    xml

--- src/main/java/pl/altkom/shop/Runner.java
+++ src/main/java/pl/altkom/shop/Runner.java
@@ -1,5 +1,6 @@
 package pl.altkom.shop;
 
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import pl.altkom.shop.service.ProductService;
@@ -7,7 +8,9 @@ import pl.altkom.shop.service.ProductService;
 public class Runner {
 
 	public static void main(String[] args) {
-		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(CoreConfig.class);
+		ApplicationContext context = new AnnotationConfigApplicationContext(CoreConfig.class);
+		// ApplicationContext context = new
+		// ClassPathXmlApplicationContext("config.xml");
 		ProductService productService = (ProductService) context.getBean("productService");
 		System.out.println(productService);
 	}
