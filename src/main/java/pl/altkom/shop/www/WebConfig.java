0d826c19a29a0e0e8e0662c38803c731c80319a4
MirekSz <m.szajowski@gmail.com>
2018-04-30 13:24:34 +0200

    locale

--- src/main/java/pl/altkom/shop/www/WebConfig.java
+++ src/main/java/pl/altkom/shop/www/WebConfig.java
@@ -5,11 +5,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
@@ -19,6 +23,11 @@ import pl.altkom.shop.lib.Profiles;
 @EnableWebMvc
 @Profile(Profiles.WEB)
 public class WebConfig extends WebMvcConfigurerAdapter {
+
+	@Bean
+	public LocaleResolver localeResolver() {// nazwa ma znaczenie
+		return new SessionLocaleResolver();
+	}
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
@@ -37,6 +46,11 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/").setViewName("index");
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new LocaleChangeInterceptor());
 	}
 
 	@Bean
